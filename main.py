371880b44c828b5e7d035758eff2c80635fa0117
mel-oezkan <meloezkan@uni-osnabureck.de>
2021-03-26 01:59:02 +0100

    testing python import structure

---
+++ main.py
@@ -0,0 +1,4 @@
+from models.beta_vae import BetaVAE
+
+model = BetaVAE()
+model.testFunc()
---
+++ models/beta_vae.py
@@ -0,0 +1,7 @@
+
+class BetaVAE:
+    def __init__(self):
+        self.test_value = 12
+
+    def testFunc(self):
+        print("I am a test function")
